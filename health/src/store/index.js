2ea0d0b07c166ea5a3f0148801feab5fefb0de9c
hzy <1054539066@qq.com>
2018-01-19 19:54:55 +0800

    添加admin

--- health/src/store/index.js
+++ health/src/store/index.js
@@ -1,13 +1,21 @@
 // store.js
 
 import hospital from './modules/hospital'
+import admin from './modules/admin'
 import Vue from 'Vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 const store = new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
   modules: {
-    hospital
+    hospital,
+    admin
   }
 })
 
